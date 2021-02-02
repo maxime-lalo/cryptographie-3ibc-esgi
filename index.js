0156efc26fe6e3e8d583c0aad3b8f380d25a439f
Maxime LALO <maxime.lalo.pro@gmail.com>
2021-02-02 11:05:39 +0100

    Initial commit

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/templates/index.html'));
+});
+
+app.post('/', function (req, res) {
+    console.log(req.body);
+    res.sendFile(path.join(__dirname + '/templates/index.html'));
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
